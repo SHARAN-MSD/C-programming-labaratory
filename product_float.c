ff6b1bcb061d97a68c152c7a25649c2939e80a5b
SHARAN-MSD <msdsharan07@gmail.com>
2024-09-30 15:11:05 +0530

    Create product_float.c

---
+++ product_float.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float a,b;
+    float product=0;
+    scanf("%f%f",&a,&b);
+    product=a*b;
+    printf("The product  of two float numbers is:%f",product);
+    return 0;
+    
+}
