3e1559117441e2a0d1f945fe02839b4fb8784cfb
SHARAN-MSD <msdsharan07@gmail.com>
2024-09-30 15:05:31 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    int sum=0;
+    printf("enter first number:");
+    scanf("%d",&a);
+    printf("enter second number:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("The sum of two numbers is:%d",sum);
+    return 0;
+    
+}
+
