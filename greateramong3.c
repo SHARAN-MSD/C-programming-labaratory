7228cd0dd0930bf4e891aeb3c8f42018dbbc1576
SHARAN-MSD <msdsharan07@gmail.com>
2024-10-06 07:10:28 -0700

    Create greateramong3.c

---
+++ greateramong3.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b)
+    {
+        if(a>c)
+        {
+            printf("A is greater");
+        }
+    } 
+    if(b>a)
+    {
+        if(b>c)
+        {
+           printf("B is greater"); 
+        }
+    }
+    if(c>a)
+    {
+        if(c>b)
+        {
+           printf("C is greater"); 
+        }
+    }
+    if(a=b)
+    {
+        if(b=c)
+        {
+            printf("all 3 numbers are equal");
+        }
+    }
+
+    return 0;
+}
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b)
+    {
+        if(a>c)
+        {
+            printf("A is greater");
+        }
+    } 
+    if(b>a)
+    {
+        if(b>c)
+        {
+           printf("B is greater"); 
+        }
+    }
+    if(c>a)
+    {
+        if(c>b)
+        {
+           printf("C is greater"); 
+        }
+    }
+    if(a=b)
+    {
+        if(b=c)
+        {
+            printf("all 3 numbers are equal");
+        }
+    }
+
+    return 0;
+}
